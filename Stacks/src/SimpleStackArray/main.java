54d7c9d00ccd5dd5db37655d692d758ccb85bf06
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-04-16 12:34:47 -0400

    Stack: ArrayStackImplementation Done

---
+++ Stacks/src/SimpleStackArray/main.java
@@ -0,0 +1,40 @@
+package SimpleStackArray;
+
+public class main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+        
+        ArrayStack stack = new ArrayStack(10);
+        
+//        stack.pop();
+        
+        stack.push(janeJones);
+        stack.push(johnDoe);
+        stack.push(marySmith);
+        stack.push(mikeWilson);
+        stack.push(billEnd);
+        stack.push(janeJones);
+        stack.push(johnDoe);
+        stack.push(marySmith);
+        stack.push(mikeWilson);
+        stack.push(billEnd);
+        
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        
+        stack.printStack();
+    }
+}
