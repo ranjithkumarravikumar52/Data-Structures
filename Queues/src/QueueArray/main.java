ba222645c56606f97037efbb0e3cf3e52bf43ab5
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-04-17 20:37:07 -0400

    Queue: Special Case created: Front of the queue is not utilized

--- Queues/src/QueueArray/main.java
+++ Queues/src/QueueArray/main.java
@@ -10,43 +10,52 @@ public class main {
         Employee billEnd = new Employee("Bill", "End", 78);
 
         QueueArray queue = new QueueArray(5);
+        
+        /*
+        Special case below:
+            Where all the empty spaces at the front are available when following the below case
+        Solution: To wrap the back of the queue to the front of the queue - DoubleEndedQueue
+        */
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        System.out.println("Removing: " + queue.remove());
 
-        queue.add(janeJones);
-        queue.add(johnDoe);
         queue.add(marySmith);
-        queue.add(mikeWilson);
-        queue.add(billEnd);
-        
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        
-        queue.add(janeJones);
-        queue.add(johnDoe);
-        queue.add(marySmith);
-        queue.add(mikeWilson);
-        queue.add(billEnd);
-        
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        
-        queue.add(janeJones);
-        queue.add(johnDoe);
-        queue.add(marySmith);
-        queue.add(mikeWilson);
-        queue.add(billEnd);
+        System.out.println("Removing: " + queue.remove());
 
-        System.out.println(queue.size());
-        System.out.println("Front: "+queue.getFront());
-        System.out.println("Back: "+queue.getBack());
+        queue.add(mikeWilson);
+        System.out.println("Removing: " + queue.remove());
+
+        queue.add(billEnd);
+        System.out.println("Removing: " + queue.remove());
+
+//        System.out.println("Removing: "+queue.remove());
+//        System.out.println("Removing: "+queue.remove());
+//        System.out.println("Removing: "+queue.remove());
+//        System.out.println("Removing: "+queue.remove());
+//        System.out.println("Removing: "+queue.remove());
+//        queue.add(janeJones);
+//        queue.add(johnDoe);
+//        queue.add(marySmith);
+//        queue.add(mikeWilson);
+//        queue.add(billEnd);
+//        
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        
+//        queue.add(janeJones);
+//        queue.add(johnDoe);
+//        queue.add(marySmith);
+//        queue.add(mikeWilson);
+//        queue.add(billEnd);
+        System.out.println("Size of the queue: " + queue.size());
+        System.out.println("Front: " + queue.getFront());
+        System.out.println("Back: " + queue.getBack());
         queue.printQueue();
-        
-        System.out.println("Peek: "+queue.peek());
 
+//        System.out.println("Peek: "+queue.peek());
     }
 }
